aaa6a2b5f339a9668ee0a7127b5eed378f3e892a
Ferdy Prasetya Putra <ferdyprasetyaputra08@gmail.com>
2026-03-03 07:30:16 +0700

    kontol

---
+++ Pertemuan2/src/MGaris.java
@@ -0,0 +1,9 @@
+/* Nama File : MGaris.java 
+ * Deskripsi : Berisi atribut dan method dalam class Garis
+ * Pembuat   : Ferdy Prasetya Putra/24060124140145
+ * Tanggal   : 24/02/2026 
+ */
+
+public class MGaris {
+    
+}
