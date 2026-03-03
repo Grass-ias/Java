1663f336c589a1a8fbc538b4e684349ad707a52f
Ferdy Prasetya Putra <ferdyprasetyaputra08@gmail.com>
2026-03-03 13:34:10 +0700

    Update MGaris.java

--- Pertemuan2/src/MGaris.java
+++ Pertemuan2/src/MGaris.java
@@ -6,8 +6,31 @@
 
 public class MGaris {
     public static void main(String[] args) {
+        Titik T1 = new Titik(0, 0);
+        Titik T2 = new Titik(4, 4);
+        Titik T3 = new Titik(0, 4);
+        Titik T4 = new Titik(4, 0);
 
+        Garis G1 = new Garis(T1, T2); 
+        Garis G2 = new Garis(T3, T4); 
+        Garis G3 = new Garis();       
 
+        System.out.print("G1: "); G1.printGaris();
+        G1.printPersamaan();
         
+        System.out.print("\nG2: "); G2.printGaris();
+        G2.printPersamaan();
+        System.out.println("Panjang G1  : " + G1.cariPanjang());
+        System.out.println("Gradien G1  : " + G1.cariGradien());
+        
+        Titik tengahG1 = G1.cariTengah();
+        System.out.print("Titik Tengah G1: "); 
+        tengahG1.printTitik();
+
+        System.out.println("Apakah G1 sejajar G3? " + G1.isSejajar(G3)); 
+        System.out.println("Apakah G1 tegak lurus G2? " + G1.isTegakLurus(G2));
+
+        System.out.println("Jumlah Objek Titik yang dibuat : " + Titik.getCounterTitik());
+        System.out.println("Jumlah Objek Garis yang dibuat : " + Garis.getCounterGaris());
     }
 }
